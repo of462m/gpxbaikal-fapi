c61b521c4da120290d65f318d30223132c211ad1
Taras I. Madzhara (gpxbaikal) <marf51@mail.ru>
2025-04-08 08:20:39 +0300

    coords test

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ from fastapi import FastAPI
 from gpxindex.index import GPXIndex
 
 app = FastAPI()
-index = GPXIndex("/home/gpxbaikal/gpxbot/mindex")
+index = GPXIndex("/var/db/mindex")
 
 @app.get("/v1/")
 async def search(tokens: str):
     return index.search(tokens)
+
+
+if __name__ == "__main__":
+    print(index.search_by_coords(53.63772,108.80937))
